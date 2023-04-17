58c42b9a7f0a48ad244903e1c2608216fb3be4f2
SirCaser <sircaser@gmail.com>
2023-04-17 15:57:45 +0200

    0.1 feature2 2.0

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        System.out.println("Feature2");
+        System.out.println("Feature2 2.0");
     }
 }
